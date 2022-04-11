1a42b76e251987e7f66623c662d4f7f1694c76e5
jintak0401 <jintak0401@naver.com>
2022-04-12 01:14:40 +0900

    fix: google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,28 +4,31 @@ import '@styles/globals.scss';
 import { PersistGate } from 'redux-persist/integration/react';
 import { createStore } from '@reduxjs/toolkit';
 import { persistStore } from 'redux-persist';
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import { MetaTags } from '@components';
 import Script from 'next/script';
+import { useRouter } from 'next/router';
+import * as gtag from '@lib/gtag';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 function MyApp({ Component, pageProps }: AppProps) {
 	const store = createStore(persistedReducer);
 	const persistor = persistStore(store);
+	const router = useRouter();
 
+	useEffect(() => {
+		const handleRouteChange = (url: URL) => {
+			/* invoke analytics function only for production */
+			if (isProduction) gtag.pageview(url);
+		};
+		router.events.on('routeChangeComplete', handleRouteChange);
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange);
+		};
+	}, [router.events]);
 	return (
 		<Fragment>
-			<Script
-				async={true}
-				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-			/>
-			<Script id="ga-analytics">
-				{`
-			     window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');	
-				`}
-			</Script>
 			<MetaTags />
 			<PersistGate loading={null} persistor={persistor}>
 				<Component {...pageProps} />
